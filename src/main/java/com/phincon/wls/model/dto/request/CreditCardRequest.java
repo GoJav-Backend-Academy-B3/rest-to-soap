ba988fcd6ea4a4198bbcf1ed6f09deb355eb70b5
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-23 10:42:05 +0700

    use builder annotation instead of manually creating buidler

--- src/main/java/com/phincon/wls/model/dto/request/CreditCardRequest.java
+++ src/main/java/com/phincon/wls/model/dto/request/CreditCardRequest.java
@@ -1,12 +1,14 @@
 package com.phincon.wls.model.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class CreditCardRequest {
     CreditCardRqHeader rqHeader;
     CreditCardRqBody rqBody;
