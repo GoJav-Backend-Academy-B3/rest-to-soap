aadd7d507360a3421fb0c3c3eaa199ee2fc565d2
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-23 13:37:35 +0700

    add javadoc

--- src/main/java/com/phincon/wls/service/CreditCardService.java
+++ src/main/java/com/phincon/wls/service/CreditCardService.java
@@ -7,13 +7,36 @@ import com.phincon.wls.model.entity.CifNumber;
 import com.phincon.wls.model.entity.CreditCard;
 
 public interface CreditCardService {
+
+    /**
+     * Queries list of credit card using CIF number.
+     *
+     * This is equivalent by calling {@code CreditCardService.queryCreditCard(String)}
+     *
+     * @param cifNumber CIF number
+     * @return List of credit card data 
+     */
     default public List<CreditCard> queryCreditCard(CifNumber cifNumber) {
         return this.queryCreditCard(cifNumber.getCif());
     }
 
+    /**
+     * Queries list of credit card using card number.
+     *
+     * This is equivalent by calling {@code CreditCardService.queryCreditCard(String)}
+     *
+     * @param cardNumber Card number
+     * @return List of credit card data 
+     */
     default public List<CreditCard> queryCreditCard(CardNumber cardNumber) {
         return this.queryCreditCard(cardNumber.getCardNumber());
     }
 
+    /**
+     * Queries list of credit card using either CIF or card number.
+     *
+     * @param number identifier number
+     * @return List of credit card data 
+     */
     public List<CreditCard> queryCreditCard(String number);
 }
