3ebec379a8aedabfc1955a1f1bf9ed439c8ef929
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-23 15:30:40 +0700

    add credit card controller test

---
+++ src/test/java/com/phincon/wls/controller/CreditCardControllerTest.java
@@ -0,0 +1,76 @@
+package com.phincon.wls.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.phincon.wls.model.entity.CifNumber;
+import com.phincon.wls.model.entity.CreditCard;
+import com.phincon.wls.service.CreditCardService;
+
+@WebMvcTest(CreditCardController.class)
+public class CreditCardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CreditCardService service;
+
+    @Autowired
+    private CreditCardController controller;
+
+    @Test
+    @Order(Integer.MIN_VALUE)
+    public void isUserControllerInjected() {
+        Assertions.assertNotNull(controller);
+    }
+
+    @Test
+    @DisplayName("POST request to /creditcard/cif should return credit cards")
+    public void requestPOSTCreditcardCif_CreditCards() {
+        List<CreditCard> cards = Arrays.asList(
+                CreditCard.builder()
+                        .cardNbr("4377001000167283").cardPrd(81).cardPrdCurr("360").cardPrdType("V")
+                        .cardAplType("P").cardSts("2").cardBlk("").cardInactiveDt("20230907")
+                        .cardCrLimit(50000000).cardAcStmBal(20080122.58).cardAcPyDueDt("20231003")
+                        .cardAcPyDueAmt(16654889.13).cardAcCurBal(33856264.81).cardAcAvailCr(16143735.19)
+                        .cardAcPointBal(238).cardEmbName("AN0341349").cardAcLstStmDt("20230913")
+                        .cardAcLstPyAmt(0).cardAcLstPyDte("00000000").cardAcCTDPyAmt(0)
+                        .cardCustomer("160323730866").cardCif("0002917054").build(),
+                CreditCard.builder()
+                        .cardNbr("4377001001587059").cardPrd(81).cardPrdCurr("360").cardPrdType("V")
+                        .cardAplType("P").cardSts("4").cardBlk("E0").cardInactiveDt("00000000")
+                        .cardCrLimit(50000000).cardAcStmBal(20080122.58).cardAcPyDueDt("20231003")
+                        .cardAcPyDueAmt(16654889.13).cardAcCurBal(33856264.81).cardAcAvailCr(16143735.19)
+                        .cardAcPointBal(238).cardEmbName("AN0341349").cardAcLstStmDt("20230913")
+                        .cardAcLstPyAmt(0).cardAcLstPyDte("00000000").cardAcCTDPyAmt(0)
+                        .cardCustomer("160323730866").cardCif("0002917054").build());
+        CifNumber cifNumber = new CifNumber("0002917054");
+        Mockito.when(service.queryCreditCard(cifNumber)).thenReturn(cards);
+
+        List<String> cifs = Arrays.asList(cifNumber.getCif(), cifNumber.getCif());
+
+        ResultActions result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/creditcard/cif/{cif}", cifNumber.getCif()));
+
+        result.andExpectAll(MockMvcResultMatchers.jsonPath("$.rsHeader").exists(),
+                MockMvcResultMatchers.jsonPath("$.rsHeader.rqHeader").exists(),
+                MockMvcResultMatchers.jsonPath("$.rsBody").exists(),
+                MockMvcResultMatchers.jsonPath("$.rsBody").isArray(),
+                MockMvcResultMatchers.jsonPath("$.rsBody[*].cif").value(Matchers.containsInAnyOrder(cifs)));
+    }
+}
