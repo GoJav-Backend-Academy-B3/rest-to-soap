cf96b02fd1b4fd5cdf2ebfe0ece83aac13ced9ca
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-24 10:41:21 +0700

    rename effect

--- src/main/java/com/phincon/wls/service/impl/CreditCardServiceImpl.java
+++ src/main/java/com/phincon/wls/service/impl/CreditCardServiceImpl.java
@@ -10,10 +10,10 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import com.phincon.wls.model.dto.creditcard.CreditCardRequest;
-import com.phincon.wls.model.dto.creditcard.CreditCardRequestBody;
-import com.phincon.wls.model.dto.creditcard.CreditCardRequestHeader;
-import com.phincon.wls.model.dto.creditcard.CreditCardResponse;
+import com.phincon.wls.model.dto.creditcard.WsCreditCardRequest;
+import com.phincon.wls.model.dto.creditcard.WsCreditCardRequestBody;
+import com.phincon.wls.model.dto.creditcard.WsCreditCardRequestHeader;
+import com.phincon.wls.model.dto.creditcard.WsCreditCardResponse;
 import com.phincon.wls.model.entity.CreditCard;
 import com.phincon.wls.service.CreditCardService;
 
@@ -30,21 +30,21 @@ public class CreditCardServiceImpl implements CreditCardService {
     public List<CreditCard> queryCreditCard(String number)  {
         
         // TODO Should change this to desired data because I don't know what to fill
-        final CreditCardRequestHeader rqHeader = CreditCardRequestHeader.builder()
+        final WsCreditCardRequestHeader rqHeader = WsCreditCardRequestHeader.builder()
                 .service("listCustomerCIFCardSummaryListInput")
                 .traceId("ABCDEFHIJKLMNOPQRSTUVWXY7")
                 .channel("CC")
                 .timestamp("2023-09-19 15:48:57.821").build();
-        final CreditCardRequest body = CreditCardRequest.builder()
+        final WsCreditCardRequest body = WsCreditCardRequest.builder()
                 .rqHeader(rqHeader)
-                .rqBody(new CreditCardRequestBody(number))
+                .rqBody(new WsCreditCardRequestBody(number))
                 .build();
 
-        final HttpEntity<CreditCardRequest> request = new HttpEntity<>(body);
-        ResponseEntity<CreditCardResponse> responseEntity = restTemplate.exchange(wsCreditCardUrl, HttpMethod.POST, request,
-                CreditCardResponse.class);
+        final HttpEntity<WsCreditCardRequest> request = new HttpEntity<>(body);
+        ResponseEntity<WsCreditCardResponse> responseEntity = restTemplate.exchange(wsCreditCardUrl, HttpMethod.POST, request,
+                WsCreditCardResponse.class);
 
-        CreditCardResponse response = responseEntity.getBody();
+        WsCreditCardResponse response = responseEntity.getBody();
 
         return response.getRsBody();
     }
