c980bb8d52812d63930423affaa57b54601ff7e1
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-24 10:31:45 +0700

    create account history dto

---
+++ src/main/java/com/phincon/wls/model/dto/accounthistory/AccountHistoryRequest.java
@@ -0,0 +1,18 @@
+package com.phincon.wls.model.dto.accounthistory;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountHistoryRequest {
+    private String acctNbr;
+    private String acctTp;
+    private String strDate;
+    private String endDate;
+    private String strIndex;
+}
---
+++ src/main/java/com/phincon/wls/model/dto/accounthistory/AccountHistoryResponse.java
@@ -0,0 +1,32 @@
+package com.phincon.wls.model.dto.accounthistory;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.phincon.wls.model.entity.Mutasi;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountHistoryResponse {
+
+    @JsonProperty("RESP")
+    private String resp;
+
+    @JsonProperty("MOREDTA")
+    private String moreDta;
+
+    @JsonProperty("LASTINDEX")
+    private String lastIndex;
+
+    @JsonProperty("MUTASI")
+    @Singular("mutasi")
+    private List<Mutasi> mutasi;
+}
