06eb802ffbb5f2753030a6f1a7badda19db98ae9
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-25 10:38:50 +0700

    add constructor annotation

--- src/main/java/com/phincon/wls/model/dto/creditcard/WsCreditCardResponse.java
+++ src/main/java/com/phincon/wls/model/dto/creditcard/WsCreditCardResponse.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import com.phincon.wls.model.entity.CreditCard;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Singular;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WsCreditCardResponse {
     private WsCreditCardResponseHeader rsHeader;
     @Singular("card") private List<CreditCard> rsBody;
