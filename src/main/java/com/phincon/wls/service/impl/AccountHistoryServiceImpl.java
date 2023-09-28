9c540f6276304b7d73d4efea75d47ab754300255
Karunia Sentosa <dibalikpohon@gmail.com>
2023-09-28 21:00:55 +0700

    handle resttempaltexception to getcause sockettimeoutexcption

--- src/main/java/com/phincon/wls/service/impl/AccountHistoryServiceImpl.java
+++ src/main/java/com/phincon/wls/service/impl/AccountHistoryServiceImpl.java
@@ -1,13 +1,19 @@
 package com.phincon.wls.service.impl;
 
+import java.net.SocketTimeoutException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.phincon.wls.exception.custom.ConnectionTimeoutException;
+import com.phincon.wls.exception.custom.NotFoundException;
 import com.phincon.wls.model.dto.accounthistory.WsAccountHistoryRequest;
 import com.phincon.wls.model.dto.accounthistory.WsAccountHistoryResponse;
 import com.phincon.wls.model.entity.Mutasi;
@@ -27,10 +33,27 @@ public class AccountHistoryServiceImpl implements AccountHistoryService {
     public List<Mutasi> queryAccountHistory(WsAccountHistoryRequest request) {
         final String urlTemplate = String.format("%s/ACCTHST/{ACCTNBR}/{ACCTTP}/{STRDATE}/{ENDDATE}/{STRINDEX}",
                 wsAccthstUrl);
-        WsAccountHistoryResponse response = restTemplate.getForObject(urlTemplate, WsAccountHistoryResponse.class, request.getAcctNbr(), request.getAcctTp(),
-                request.getStrDate(), request.getEndDate(), request.getStrIndex());
-        
-        List<Mutasi> mutasi = response.getMutasi();
+        ResponseEntity<WsAccountHistoryResponse> responseEntity = null;
+        try {
+            responseEntity = restTemplate.getForEntity(urlTemplate,
+                    WsAccountHistoryResponse.class, request.getAcctNbr(), request.getAcctTp(),
+                    request.getStrDate(), request.getEndDate(), request.getStrIndex());
+        } catch (RestClientException e) {
+            // SocketTimeoutException is the impostor
+            if (e.getCause() instanceof SocketTimeoutException) {
+                throw new ConnectionTimeoutException(String.format("Connection to service timed out.", wsAccthstUrl));
+            }
+            throw e;
+        }
+
+        if (responseEntity.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+            throw new NotFoundException(
+                    String.format("Parameter not found: (ACCTNBR=%s, ACCTTP=%s, STRDATE=%s, ENDDATE=%s, STRINDEX=%s)",
+                            request.getAcctNbr(), request.getAcctTp(),
+                            request.getStrDate(), request.getEndDate(), request.getStrIndex()));
+        }
+
+        List<Mutasi> mutasi = responseEntity.getBody().getMutasi();
 
         return mutasi;
     }
